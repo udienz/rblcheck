d73d784885c5885f9981e4291e33463cd567dfb9
Mahyuddin Susanto <udienz@gmail.com>
2014-01-20 10:11:45 +0700

    Imported Debian patch 20020316-7ubuntu1+ppa2

---
+++ config.h
@@ -0,0 +1,8 @@
+#define STDC_HEADERS
+#define HAVE_GETOPT_H
+#define HAVE_UNISTD_H
+
+#define PACKAGE "rblcheck"
+#define VERSION "1.5-20020316"
+
+#define _GNU_SOURCE
--- sites.h
+++ sites.h
@@ -62,3 +62,13 @@
 /* ORDB: Open Relay DataBase
    http://www.ordb.org/ */
 /* SITE("relays.ordb.org") */
+
+
+SITE("sbl.spamhaus.org");
+SITE("xbl.spamhaus.org");
+SITE("pbl.spamhaus.org");
+SITE("bl.spamcop.net");
+/*SITE("psbl.surriel.com");
+SITE("dnsbl.njabl.org");*/
+SITE("dnsbl.sorbs.net");
+SITE("b.barracudacentral.org");
